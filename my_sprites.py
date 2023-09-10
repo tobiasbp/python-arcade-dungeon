a647d5dbbc2250a5926134308db7abcd181f5a62
tobiasbp <tobiasbp@gmail.com>
2023-09-10 21:17:52 +0200

    Fix: Wrong paths to png files

--- my_sprites.py
+++ my_sprites.py
@@ -19,7 +19,7 @@ class Player(arcade.Sprite):
         super().__init__(
             center_x=center_x,
             center_y=center_y,
-            filename="images/tiny_dungeon/tiles/tile_0109.png",
+            filename="images/tiny_dungeon/Tiles/tile_0109.png",
             scale=scale,
         )
 
@@ -54,7 +54,7 @@ class PlayerShot(arcade.Sprite):
             center_x=center_x,
             center_y=center_y,
             scale=scale,
-            filename="images/tiny_dungeon/tiles/tile_0109.png",
+            filename="images/tiny_dungeon/Tiles/tile_0109.png",
             flipped_diagonally=True,
             flipped_horizontally=True,
             flipped_vertically=False,
